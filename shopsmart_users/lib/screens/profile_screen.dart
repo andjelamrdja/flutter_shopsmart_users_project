d04e5b24022662eb494510e19d815adb072ff4cb
Andjela Mrdja <EC-PC@DESKTOP-8RJU8OL>
2025-03-04 20:25:11 +0100

    Created components on profile page

--- shopsmart_users/lib/screens/profile_screen.dart
+++ shopsmart_users/lib/screens/profile_screen.dart
@@ -1,4 +1,11 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:provider/provider.dart';
+import 'package:shopsmart_users/providers/theme_provider.dart';
+import 'package:shopsmart_users/services/assets_manager.dart';
+import 'package:shopsmart_users/widgets/subtitle_text.dart';
 import 'package:shopsmart_users/widgets/title_text.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -6,10 +13,173 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: TitlesTextWidget(label: "Profile Screen"),
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    return Scaffold(
+        appBar: AppBar(
+            leading: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                AssetsManager.shoppingCart,
+              ),
+            ),
+            title: Text("Profile")), //AppBar je gornja traka
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Visibility(
+              visible: false,
+              child: Padding(
+                padding: EdgeInsets.all(18.0),
+                child: TitlesTextWidget(
+                    label: "Please login to have unlimited access"),
+              ),
+            ),
+            Visibility(
+              visible: true,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 60,
+                      height: 60,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Theme.of(context).cardColor,
+                        border: Border.all(
+                          color: Theme.of(context).colorScheme.background,
+                          width: 3,
+                        ),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TitlesTextWidget(label: "Andjela LaLaLa"),
+                        SizedBox(
+                          height: 6,
+                        ),
+                        SubtitleTextWidget(label: "test@test.com")
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const TitlesTextWidget(
+                    label: "General",
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  CustomListTile(
+                    text: "All Orders",
+                    imagePath: AssetsManager.orderSvg,
+                    function: () {},
+                  ),
+                  CustomListTile(
+                    text: "Wishlist",
+                    imagePath: AssetsManager.wishlistSvg,
+                    function: () {},
+                  ),
+                  CustomListTile(
+                    text: "Viewed Recently",
+                    imagePath: AssetsManager.recent,
+                    function: () {},
+                  ),
+                  CustomListTile(
+                    text: "Address",
+                    imagePath: AssetsManager.address,
+                    function: () {},
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  const Divider(
+                    thickness: 1,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  const TitlesTextWidget(
+                    label: "Settings",
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  SwitchListTile(
+                      secondary: Image.asset(
+                        AssetsManager.theme,
+                        height: 34,
+                      ),
+                      title: Text(themeProvider.getIsDarkTheme
+                          ? "Dark Mode"
+                          : "Light Mode"),
+                      value: themeProvider.getIsDarkTheme,
+                      onChanged: (value) {
+                        themeProvider.setDarkTheme(themeValue: value);
+                        log("Theme state ${themeProvider.getIsDarkTheme}");
+                      }),
+                ],
+              ),
+            ),
+            Center(
+              child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      30.0,
+                    ),
+                  ),
+                ),
+                onPressed: () {},
+                icon: const Icon(Icons.login),
+                label: const Text("Login"),
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class CustomListTile extends StatelessWidget {
+  const CustomListTile(
+      {super.key,
+      required this.imagePath,
+      required this.text,
+      required this.function});
+
+  final String imagePath, text;
+  final Function function;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        function();
+      },
+      title: SubtitleTextWidget(label: text),
+      leading: Image.asset(
+        imagePath,
+        height: 34,
       ),
+      trailing: const Icon(IconlyLight.arrowRight2),
     );
   }
 }
